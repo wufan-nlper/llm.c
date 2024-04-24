557420978a826d233da8734a633b01630acc1969
wufan <wufan@xueqiu.com>
2024-04-24 15:23:38 +0800

    add device parser

--- train_gpt2.py
+++ train_gpt2.py
@@ -337,6 +337,7 @@ if __name__ == "__main__":
     parser.add_argument("--num_iterations", type=int, default=10, help="number of iterations to run")
     parser.add_argument("--batch_size", type=int, default=4, help="batch size")
     parser.add_argument("--sequence_length", type=int, default=64, help="sequence length")
+    parser.add_argument("--device", type=str, default="cpu", help="choose a device")
     args = parser.parse_args()
     B, T = args.batch_size, args.sequence_length
     assert 1 <= T <= 1024
